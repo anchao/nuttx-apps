06be505fba1ce47342fbb67ba1b5c7e1b4e095e4
chao an <anchao@xiaomi.com>
2022-08-31 23:05:38 +0800

    examples/usrsocktest: add some delay to wait the daemon task ready
    Signed-off-by: chao an <anchao@xiaomi.com>

--- examples/usrsocktest/usrsocktest_daemon.c
+++ examples/usrsocktest/usrsocktest_daemon.c
@@ -2063,6 +2063,12 @@ errout_closepipe:
 out:
   pthread_mutex_unlock(&daemon_mutex);
   usrsocktest_dbg("ret: %d\n", ret);
+
+  if (ret == OK)
+    {
+      usleep(100);
+    }
+
   return ret;
 }
 
@@ -2338,6 +2344,11 @@ bool usrsocktest_send_delayed_command(const char cmd,
   while (sem_wait(&delayed_cmd->startsem) != OK);
 
   sq_addlast(&delayed_cmd->node, &priv->delayed_cmd_threads);
+
+  if (ret == OK)
+    {
+      usleep(100);
+    }
 
   return true;
 }
